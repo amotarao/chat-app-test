9e383172e5d7ca5285335d959b5b221b390bd288
amotarao <amon@amotarao.me>
2016-06-28 08:40:40 +0900

    Update Setting

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
-var gulp = require('gulp');
-var jade = require('gulp-jade');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var minifyCss = require('gulp-minify-css');
-var plumber = require('gulp-plumber');
-var webserver = require('gulp-webserver');
+var gulp = require('gulp')
+var jade = require('gulp-jade')
+var sass = require('gulp-sass')
+var autoprefixer = require('gulp-autoprefixer')
+var minifyCss = require('gulp-minify-css')
+var plumber = require('gulp-plumber')
+var replace = require('gulp-replace')
+var webserver = require('gulp-webserver')
 
 
 /*
@@ -13,9 +14,16 @@ var webserver = require('gulp-webserver');
 
 gulp.task('jade', function() {
   gulp.src('./app/jade/**/*.jade')
+    .pipe(replace('<SERVER_HOST>', 'localhost'))
+    .pipe(replace('<SERVER_PORT>', '9000'))
     .pipe(jade())
-    .pipe(gulp.dest('./dist/'))
-});
+    .pipe(gulp.dest('./dist_local/'))
+  gulp.src('./app/jade/**/*.jade')
+    .pipe(replace('<SERVER_HOST>', '54.197.222.15'))
+    .pipe(replace('<SERVER_PORT>', '9000'))
+    .pipe(jade())
+    .pipe(gulp.dest('./dist_server/'))
+})
 
 
 /*
@@ -28,8 +36,9 @@ gulp.task('sass', function(){
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(autoprefixer("last 3 version"))
     .pipe(minifyCss())
-    .pipe(gulp.dest('./dist/css/'));
-});
+    .pipe(gulp.dest('./dist_local/css/'))
+    .pipe(gulp.dest('./dist_server/css/'))
+})
 
 
 /*
@@ -38,8 +47,41 @@ gulp.task('sass', function(){
 
 gulp.task('copy-html', function(){
   gulp.src('./app/html/**/*.html')
-    .pipe(gulp.dest('./dist/'));
-});
+    .pipe(gulp.dest('./dist_local/'))
+    .pipe(gulp.dest('./dist_server/'))
+})
+
+
+/*
+ *  ['copy-js'] ... Copy js files.
+ */
+
+gulp.task('copy-js', function(){
+  gulp.src('./app/js/**/*.js')
+    .pipe(replace('<SERVER_HOST>', 'localhost'))
+    .pipe(replace('<SERVER_PORT>', '9000'))
+    .pipe(gulp.dest('./dist_local/js/'))
+  gulp.src('./app/js/**/*.js')
+    .pipe(replace('<SERVER_HOST>', '54.197.222.15'))
+    .pipe(replace('<SERVER_PORT>', '9000'))
+    .pipe(gulp.dest('./dist_server/js/'))
+})
+
+
+/*
+ *  ['copy-api'] ... Copy html files.
+ */
+
+gulp.task('copy-api', function(){
+  gulp.src('./api/**/*')
+    .pipe(replace('<SERVER_HOST>', 'localhost'))
+    .pipe(replace('<SERVER_PORT>', '9000'))
+    .pipe(gulp.dest('./dist_local/api/'))
+  gulp.src('./api/**/*')
+    .pipe(replace('<SERVER_HOST>', '54.197.222.15'))
+    .pipe(replace('<SERVER_PORT>', '9000'))
+    .pipe(gulp.dest('./dist_server/api/'))
+})
 
 
 /*
@@ -49,13 +91,13 @@ gulp.task('copy-html', function(){
  */
 
 gulp.task('webserver', function () {
-  gulp.src('./dist/')
+  gulp.src('./dist_local/')
     .pipe(webserver({
       host: 'localhost',
-      port: 9000,
+      port: 8081,
       livereload: true
-    }));
-});
+    }))
+})
 
 
 
@@ -66,10 +108,11 @@ gulp.task('webserver', function () {
  */
 
 gulp.task('dev', ['webserver'], function(){
-  gulp.watch('./app/jade/**/*.jade', ['jade']);
-  gulp.watch('./app/sass/**/*.scss', ['sass']);
-  gulp.watch('./app/html/**/*.html', ['copy-html']);
-});
+  gulp.watch('./app/jade/**/*.jade', ['jade'])
+  gulp.watch('./app/sass/**/*.scss', ['sass'])
+  gulp.watch('./app/html/**/*.html', ['copy-html'])
+  gulp.watch('./app/js/**/*.js', ['copy-js'])
+})
 
 
 /*
@@ -78,7 +121,7 @@ gulp.task('dev', ['webserver'], function(){
 
 gulp.task('install', function () {
   gulp.src('./node_modules/sanitize.css/lib/sanitize.scss')
-    .pipe(gulp.dest('./app/sass/lib/'));
+    .pipe(gulp.dest('./app/sass/lib/'))
 //  gulp.src(['./node_modules/bulma/bulma.sass', './node_modules/bulma/bulma/**'], { base: './node_modules/bulma'} )
-//    .pipe(gulp.dest('./dev/sass/lib/'));
-});
+//    .pipe(gulp.dest('./dev/sass/lib/'))
+})
